f5deef901cad27481d689002e0fc76c16ad13dee
moviesdriveformovie <159187589+moviesdriveformovie@users.noreply.github.com>
2024-04-06 01:54:16 +0530

    Create starknet.sh

---
+++ .github/workflows/starknet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-b230fd13-d09f-4d3b-bfce-f1a5eae66d61/"
+
+OUTPUT_DIR="logs"
+
+starknet_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"starknet_blockNumber"}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$starknet_data" >> "$OUTPUT_DIR/starknet.txt"
+    
+    echo "StarkNet data fetched and saved to $OUTPUT_DIR/starknet.txt."
+else
+    echo "Failed to fetch StarkNet data."
+fi
