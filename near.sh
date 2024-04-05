72320c522de6d5ccd998ea5374a27c3d2e40a28d
moviesdriveformovie <159187589+moviesdriveformovie@users.noreply.github.com>
2024-04-06 02:05:12 +0530

    Create near.sh

---
+++ near.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://near.lava.build/lava-referer-b230fd13-d09f-4d3b-bfce-f1a5eae66d61/"
+
+OUTPUT_DIR="logs"
+
+near_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"block","id":"HappyCuanAirdrop","params":{"finality":"final"}}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$near_data" >> "$OUTPUT_DIR/near.txt"
+    
+    echo "NEAR data fetched and saved to $OUTPUT_DIR/near.txt."
+else
+    echo "Failed to fetch NEAR data."
+fi
